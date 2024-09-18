11bd705b4e78547a023ea0aa0980cb9f08e4920f
Mostaza5330 <128562434+Mostaza5330@users.noreply.github.com>
2024-09-17 21:26:28 -0700

    Comienzo

---
+++ ProyectoDdATicket/src/main/java/com/mycompany/proyectoddaticket/ProyectoDdATicket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoddaticket;
+
+/**
+ *
+ * @author osval
+ */
+public class ProyectoDdATicket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
