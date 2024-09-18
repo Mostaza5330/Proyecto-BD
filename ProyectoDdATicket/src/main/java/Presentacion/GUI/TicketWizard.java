72b8738ef7b6b5b96f01fac56966bb5842d06b6f
Mostaza5330 <128562434+Mostaza5330@users.noreply.github.com>
2024-09-17 21:47:00 -0700

    conector jdbc driver

--- ProyectoDdATicket/src/main/java/Presentacion/GUI/TicketWizard.java
+++ ProyectoDdATicket/src/main/java/Presentacion/GUI/TicketWizard.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.ticketwizard;
+package Presentacion.GUI;
 
 import DATOS.Conexion;
 import java.sql.Connection;
